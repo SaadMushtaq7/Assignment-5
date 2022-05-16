5579d16e79d3550a753b2b8b6588b3b8780cc5d1
SaadMushtaq7 <saad.mushtaq@emumba.com>
2022-05-16 12:39:22 +0500

    10th commit before review

--- client/src/utils/DialogBox.js
+++ client/src/utils/DialogBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
